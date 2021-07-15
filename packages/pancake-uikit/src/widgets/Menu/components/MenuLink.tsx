2a2f09dc613d8992db2736420537b23bed4d529f
David Wei <yunze.wei@edu.sait.ca>
2021-07-15 15:37:53 -0600

    changed for trade hyperlinks to open up in a new window

--- packages/pancake-uikit/src/widgets/Menu/components/MenuLink.tsx
+++ packages/pancake-uikit/src/widgets/Menu/components/MenuLink.tsx
@@ -1,13 +1,21 @@
-import React, { AnchorHTMLAttributes } from "react";
+import React, {AreaHTMLAttributes, AnchorHTMLAttributes } from "react";
 import { NavLink } from "react-router-dom";
 
-const MenuLink: React.FC<AnchorHTMLAttributes<HTMLAnchorElement>> = ({ href, ...otherProps }) => {
+ /* have to make sure our href hyperlinks in the frontend
+ that we do not want to open in a new window ends with s or else it will break*/
+const MenuLink: React.FC<AreaHTMLAttributes<HTMLAreaElement>> = ({ target, href, ...otherProps }) => {
   const isHttpLink = href?.startsWith("http");
+  // new
+  const endWith = href?.endsWith("s")
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const Tag: any = isHttpLink ? "a" : NavLink;
-  const props = isHttpLink ? { href } : { to: href };
-  return <Tag role="button" {...props} {...otherProps} />;
+  const props = isHttpLink ? { href } : { to: href }
+  // new
+  const newWindow = endWith? "_self" : "_blank"
+  
+  // delete target if it does not work
+  return <Tag role="button" target={newWindow} {...props} {...otherProps} />;
 };
 
 export default MenuLink;
