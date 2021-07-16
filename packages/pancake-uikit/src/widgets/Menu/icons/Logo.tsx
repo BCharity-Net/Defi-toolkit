2fea749475a941e2f87c2b544c7d0bd3df384e5b
sliu7714 <siyuanliu3@gmail.com>
2021-07-16 09:19:31 -0600

    Update logo and social links
    logos are using the pngs in the frontend public/images folder

--- packages/pancake-uikit/src/widgets/Menu/icons/Logo.tsx
+++ packages/pancake-uikit/src/widgets/Menu/icons/Logo.tsx
@@ -9,9 +9,16 @@ interface LogoProps extends SvgProps {
 const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
   const textColor = isDark ? "#FFFFFF" : "#000000";
   return (
-    <Svg viewBox="0 0 160 26" {...props}>
-      <image width="32" height="32" href={require("../../../resources/hands.png")}/>
-      <image className="heart" x="8.5" y="2" width="14" height="14" href={require("../../../resources/heart.png")}/>
+    <Svg viewBox="0 0 32 32" {...props}>
+        {/*  "images" in the path refers to the images folder in the frontend - won't load in storybook*/}
+        <image width="32" height="32" href="/images/test-uikit/hands.png"/>
+        <image className="heart" x="9" y="2" width="13" height="13" href="/images/test-uikit/heart.png"/>
+
+
+        {/* comment this out to test images in storybook - DONT include in published version */}
+        {/*<image width="32" height="32" href={require("../../../resources/hands.png")}/>*/}
+        {/*<image className="heart" x="9" y="2" width="13" height="13"*/}
+        {/*       href={require("../../../resources/heart.png")}/>*/}
     </Svg>
   );
 };
