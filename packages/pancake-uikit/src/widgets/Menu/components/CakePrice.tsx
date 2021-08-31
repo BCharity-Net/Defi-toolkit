94db75598a3c1ab1efe7021b034d983b0d6bb6f9
David Wei <yunze.wei@edu.sait.ca>
2021-08-31 11:51:08 -0600

    version 4.4.2 changed price icon link

--- packages/pancake-uikit/src/widgets/Menu/components/CakePrice.tsx
+++ packages/pancake-uikit/src/widgets/Menu/components/CakePrice.tsx
@@ -24,10 +24,7 @@ const PriceLink = styled.a`
 // add link and token price here
 const CakePrice: React.FC<Props> = ({ cakePriceUsd }) => {
   return cakePriceUsd ? (
-    <PriceLink
-      href="https://quickchart.app/token/0x9bbcda2606e616659b118399a2823e8a392f55da?pairedWith=USDC"
-      target="_blank"
-    >
+    <PriceLink href="https://info.pangolin.exchange/#/pair/0x390c68676ce4556a190d1cfcae6253a3d1e728ce" target="_blank">
       <PancakesIcon width="24px" mr="8px" />
       <Text color="textSubtle" bold>{`$${cakePriceUsd.toFixed(3)}`}</Text>
     </PriceLink>
